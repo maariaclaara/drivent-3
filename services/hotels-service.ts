fe02d8da29cf57d87a5d45333ca43c9602f74046
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-11-20 17:09:54 -0300

    fix

---
+++ services/hotels-service.ts
@@ -0,0 +1,40 @@
+import hotelRepository from '@/repositories/hotels-repository';
+import { enrollmentRepository } from '@/repositories/enrollments-repository';
+import { notFoundError } from '@/errors';
+import { ticketsRepository } from '@/repositories/tickets-repository';
+import { cannotListHotelsError } from '@/errors/cannot-list-hotels-error';
+
+async function listHotels(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+
+  if (!ticket || ticket.status === 'RESERVED' || ticket.ticketTypeId || !ticket.ticketTypeId) {
+    throw cannotListHotelsError();
+  }
+}
+
+async function getHotels(userId: number) {
+  await listHotels(userId);
+
+  const hotels = await hotelRepository.findHotels();
+  return hotels;
+}
+
+async function getHotelsWithRooms(userId: number, hotelId: number) {
+  await listHotels(userId);
+
+  const hotel = await hotelRepository.findRoomsByHotelId(hotelId);
+
+  if (!hotel) {
+    throw notFoundError();
+  }
+  return hotel;
+}
+
+export default {
+  getHotels,
+  getHotelsWithRooms,
+};
